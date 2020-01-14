163c801566729ca4464f2e0ab20eaac6c015eec8
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-01-14 11:31:36 +0300

    added class in forms.py

--- pitching/posts/forms.py
+++ pitching/posts/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators = [DataRequired(), Length(min = 2, max = 45)])
+    content = TextAreaField('Pitch', validators = [DataRequired()])
+    category = SelectField('Pitch Category', choices=[('Inspirational','Inspirational'),('Biography','Biography'),('Business','Business'),('Ideas','Ideas')])
+    submit = SubmitField('Post')
