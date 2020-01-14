6d3dc18bf72a60b92587bbe89acf4a969d1fb18f
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-01-14 11:19:01 +0300

    imported flask

---
+++ pitching/main/routes.py
@@ -0,0 +1,15 @@
+rom flask import render_template, request, Blueprint
+from flaskblog.models import Pitch
+
+main = Blueprint('main',__name__)
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    pitches = Pitch.query.order_by(Pitch.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', pitches = pitches)
+
+@main.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
