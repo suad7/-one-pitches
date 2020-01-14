a4ca89d12b72cac246bf20be59e8b75ee09cddac
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-01-14 11:45:28 +0300

    added function for  update posts in routes.py

--- pitching/posts/routes.py
+++ pitching/posts/routes.py
@@ -25,4 +25,22 @@ def post(post_id):
     post = Pitch.query.get_or_404(post_id)
     return render_template('post.html', title = post.title, post = post)
 
+@posts.route("/post/<int:post_id>/update", methods= ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Pitch.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'successfully')
+        return redirect(url_for('posts.post', post_id =  post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',title = 'Update Post', form = form, legend = 'Update Post')
+
 
