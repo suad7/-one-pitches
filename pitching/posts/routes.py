1b53b1cb2230f943770ea04f2866a4f41911c4e8
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-01-14 11:47:38 +0300

    added function for  delete posts in routes.py

--- pitching/posts/routes.py
+++ pitching/posts/routes.py
@@ -44,3 +44,16 @@ def update_post(post_id):
     return render_template('create_post.html',title = 'Update Post', form = form, legend = 'Update Post')
 
 
+@posts.route("/post/<int:post_id>/delete", methods= ['POST'])
+@login_required
+def delete_post(post_id):
+    post = Pitch.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'successfully')
+    return redirect(url_for('main.home'))
+
+
+
